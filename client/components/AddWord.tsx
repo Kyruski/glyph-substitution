9a8503116d80c525de9165cdf023148fbea89580
kyruski <jamescboyett@gmail.com>
2019-03-12 16:09:02 -0500

    Removed some unneeded imports

--- client/components/AddWord.tsx
+++ client/components/AddWord.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 function AddWord(props: any): JSX.Element {
   let [message, setMessage] = useState("");
