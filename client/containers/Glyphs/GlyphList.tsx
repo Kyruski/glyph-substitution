a7788e02fca30880ad0307227c3d536f419b5155
James Boyett <44087679+Kyruski@users.noreply.github.com>
2019-11-11 19:38:55 -0800

    AntD Table Conversion (#10)
    * Converts the Bot Table with all active bots to an AntD component, adds styling
    
    * Changes the remove bot button an AntD button component; Moved button styling to global style.css
    
    * Changes the Glyph List Table to an AntD Table
    
    * Changes the Words List Table to an AntD Component

--- client/containers/Glyphs/GlyphList.tsx
+++ client/containers/Glyphs/GlyphList.tsx
@@ -1,31 +1,37 @@
 import React from "react";
-import GlyphListItem from "../../components/Glyphs/GlyphListItem";
 import makeLettersList from "../../../lib/makeLettersList";
 import store from "../../store";
+import { Table } from "antd";
 
 function GlyphList(): JSX.Element {
   const lettersList = makeLettersList(store.getState().glyphList);
+  const columns = [
+    {
+      title: "Letter",
+      dataIndex: "letter",
+      key: "letter"
+    },
+    {
+      title: "Substitution Glyphs",
+      dataIndex: "subGlyphs",
+      key: "subGlyphs"
+    }
+  ];
+  const dataSource = lettersList.map(
+    (item: [string, Array<string>], index: number) => ({
+      key: index,
+      letter: item[0],
+      subGlyphs: item[1].join(", ")
+    })
+  );
 
   return (
-    <div>
-      {/* 
-          //@ts-ignore */}
-      <table border="1" style={{ maxWidth: "80%", margin: "0 2%" }}>
-        <thead>
-          <tr>
-            <th>Letter</th>
-            <th>Substitution Glyphs</th>
-          </tr>
-        </thead>
-        <tbody>
-          {lettersList.map(
-            (item: [string, Array<string>], index: number): JSX.Element => (
-              <GlyphListItem key={`glyphlistitem${index}`} item={item} />
-            )
-          )}
-        </tbody>
-      </table>
-    </div>
+    <Table
+      dataSource={dataSource}
+      columns={columns}
+      pagination={false}
+      className="glyph-table"
+    />
   );
 }
 
